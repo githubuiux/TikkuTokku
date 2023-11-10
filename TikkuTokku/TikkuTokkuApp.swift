2f3732bc7f8510854e4b92638b2864f71b0caa36
githubuiux <uiuxista@gmail.com>
2023-11-10 14:30:28 +0300

    Initial Commit

---
+++ TikkuTokku/TikkuTokkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikkuTokkuApp.swift
+//  TikkuTokku
+//
+//  Created by Levent on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikkuTokkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
